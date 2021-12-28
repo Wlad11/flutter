88f63486cc4e57526d61bb6c3e48b5b81f7361b0
Wlad11 <dds11malkoff@yandex.ru>
2021-12-28 20:51:39 +0700

    start

---
+++ lesson/lib/widgets/aut/aut_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AutWidget extends StatelessWidget {
+  const AutWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
