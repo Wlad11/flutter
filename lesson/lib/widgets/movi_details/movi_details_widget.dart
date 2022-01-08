4214070c01c661d7c6787b5417983db54296e4f6
Wlad11 <dds11malkoff@yandex.ru>
2022-01-08 18:56:36 +0700

    Начал делать экран Фильма

---
+++ lesson/lib/widgets/movi_details/movi_details_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MoviDetailsWidget extends StatefulWidget {
+  const MoviDetailsWidget({ Key? key }) : super(key: key);
+
+  @override
+  _MoviDetailsWidgetState createState() => _MoviDetailsWidgetState();
+}
+
+class _MoviDetailsWidgetState extends State<MoviDetailsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
