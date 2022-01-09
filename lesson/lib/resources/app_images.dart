02627a02005b6847ab2a954adad1646fc3381876
Wlad11 <dds11malkoff@yandex.ru>
2022-01-10 00:22:45 +0700

    Добавил картинок

--- lesson/lib/resources/app_images.dart
+++ lesson/lib/resources/app_images.dart
@@ -4,4 +4,9 @@ class AppImages {
   AppImages._();
 
   static const String film = 'images/film.jpg';
+  static const String devushka = 'images/devushka.jpg';
+  static const String devushka_3 = 'images/devushka_3.jpg';
+  static const String devushka1 = 'images/devushka1.jpg';
+  static const String nakidke2 = 'images/nakidke2.jpg';
+  static const String voin = 'images/voin.jpg';
 }
